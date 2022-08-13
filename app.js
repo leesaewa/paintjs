66cb82eacc92f60605cead16a879b2f70fe6eda6
leesaewa <saewa0424@gmail.com>
2022-08-13 15:39:42 +0900

    painting color line

--- app.js
+++ app.js
@@ -6,18 +6,26 @@ canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
 
-//사각형을 채움. x, y, width, height
-// ctx.fillStyle = "red";
-// ctx.fillRect(50, 50, 100, 200);
-// fillRect, Rect는 지름길임. 지름길을 사용하고 싶지 않다면, moveTo, lineTo로 하나씩 그려나갈 수 있음.
+// 색상이 변함
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#c56cf0",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#7efff5",
+  "#18dcff",
+  "#7d5fff",
+];
 
-// 집 그리기
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
-ctx.fill();
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onClick);
